ecc61c511dc6ad48845d679baa1b1e75dc3bf244
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-08-16 01:43:45 +0300

    repeatedWord

--- Data-Structures/HashTable/lib/src/main/java/HashTable/HashNode.java
+++ Data-Structures/HashTable/lib/src/main/java/HashTable/HashNode.java
@@ -3,19 +3,5 @@ package HashTable;
 
 
 public class HashNode {
-    public String key;
-    public String value;
 
-    public HashNode(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "HashNode{" +
-                "key='" + key + '\'' +
-                ", value='" + value + '\'' +
-                '}';
-    }
 }
